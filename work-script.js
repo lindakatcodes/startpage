ecebfdc308667d1ecc0170cde296de8e45f57f60
LindaKat <lindakt16@gmail.com>
2023-01-22 14:16:51 -0600

    add more links, adjust styles

--- work-script.js
+++ work-script.js
@@ -3,10 +3,10 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 /**
  * Each link should have a url, a name to display, and a type to control the display coloring
  * Default shape: 
- * url - string
  * name - string
  * abbr - string (2 letter abbreviation for display)
- * type - string
+ * type - string 
+ * url - string
  * 
  * Available types:
  * core - general links I regularly go to 
@@ -14,25 +14,30 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
  * tool - tools for a specific use
  * learn - resources for learning
  * 
- * { url: '', name: '', abbr: '', type: '' },
+ * { name: '', abbr: '', type: '', url: '' },
  */
 const links = [
-  { url: 'https://www.youtube.com/', name: 'YouTube', abbr: 'Yt', type: 'core' },
-  { url: 'https://www.twitch.tv/', name: 'Twitch', abbr: 'Tw', type: 'core' },
-  { url: 'https://www.excalidraw.com/', name: 'Excalidraw', abbr: 'Ex', type: 'core' },
-  { url: 'https://www.notion.com/', name: 'Notion', abbr: 'No', type: 'core' },
-  { url: 'https://coolors.co/generate', name: 'Palatte Generator', abbr: 'Cp', type: 'tool' },
-  { url: 'https://colorable.jxnblk.com/', name: 'Contrast Check', abbr: 'Cc', type: 'tool' },
-  { url: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/', name: 'Flexbox Guide', abbr: 'Fb', type: 'reference' },
-  { url: 'https://css-tricks.com/snippets/css/complete-guide-grid/', name: 'Grid Guide', abbr: 'Gr', type: 'reference' },
-  { url: 'https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet', name: 'Markdown', abbr: 'Md', type: 'reference' },
-  { url: 'https://www.freecodecamp.org/learn', name: 'FreeCodeCamp', abbr: 'Fc', type: 'learn' },
-  { url: 'https://adventofcode.com/', name: 'Advent of Code', abbr: 'Ac', type: 'learn' },
-  { url: 'https://regexr.com/', name: 'RegExr', abbr: 'Re', type: 'tool' },
-  { url: 'https://www.a11yproject.com/checklist/', name: 'A11y Checklist', abbr: 'Al', type: 'reference' },
-  { url: 'https://vuejs.org/guide/quick-start.html', name: 'Vue Docs', abbr: 'Vu', type: 'reference' },
-  { url: 'https://reactjs.org/docs/getting-started.html', name: 'React Docs', abbr: 'Re', type: 'reference' },
-  { url: 'https://angular.io/docs', name: 'Angular Docs', abbr: 'Ng', type: 'reference' },
+  { name: 'YouTube', abbr: 'Yt', type: 'core', url: 'https://www.youtube.com/' },
+  { name: 'Twitch', abbr: 'Tw', type: 'core', url: 'https://www.twitch.tv/' },
+  { name: 'Excalidraw', abbr: 'Ex', type: 'core', url: 'https://www.excalidraw.com/' },
+  { name: 'Notion', abbr: 'No', type: 'core',  url: 'https://www.notion.com/' },
+  { name: 'Color Palatte Generator', abbr: 'Cp', type: 'tool',  url: 'https://www.coolors.co/generate' },
+  { name: 'Color Contrast Checker', abbr: 'Cc', type: 'tool', url: 'https://www.colorable.jxnblk.com/' },
+  { name: 'Flexbox Guide', abbr: 'Fb', type: 'reference', url: 'https://www.css-tricks.com/snippets/css/a-guide-to-flexbox/' },
+  { name: 'Grid Guide', abbr: 'Gr', type: 'reference', url: 'https://www.css-tricks.com/snippets/css/complete-guide-grid/' },
+  { name: 'Markdown Cheatsheet', abbr: 'Md', type: 'reference', url: 'https://www.github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet' },
+  { name: 'FreeCodeCamp', abbr: 'Fc', type: 'learn', url: 'https://www.freecodecamp.org/learn' },
+  { name: 'Advent of Code', abbr: 'Ac', type: 'learn', url: 'https://www.adventofcode.com/' },
+  { name: 'RegExr', abbr: 'Re', type: 'tool', url: 'https://www.regexr.com/' },
+  { name: 'A11y Checklist', abbr: 'Al', type: 'reference', url: 'https://www.a11yproject.com/checklist/' },
+  { name: 'Vue Docs', abbr: 'Vu', type: 'reference',  url: 'https://www.vuejs.org/guide/quick-start.html' },
+  { name: 'React Docs', abbr: 'Re', type: 'reference', url: 'https://www.reactjs.org/docs/getting-started.html' },
+  { name: 'Angular Docs', abbr: 'Ng', type: 'reference',  url: 'https://www.angular.io/docs' },
+  { name: 'TypeScript Docs', abbr: 'Ts', type: 'reference', url: 'https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html' },
+  { name: 'Egghead', abbr: 'Eg', type: 'learn',  url: 'https://www.egghead.io/' },
+  { name: 'FrontEnd Masters', abbr: 'Fm', type: 'learn', url: 'https://www.frontendmasters.com/dashboard/' },
+  { name: 'Joy of React', abbr: 'Jr', type: 'learn', url: '' },
+
 ]
 
 createApp({
