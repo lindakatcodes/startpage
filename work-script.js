ad54efe9f1e79b4b24b2d7c582a880515dfde9c9
LindaKat <lindakt16@gmail.com>
2023-01-23 12:53:32 -0600

    change vue script to prod version

--- work-script.js
+++ work-script.js
@@ -1,4 +1,4 @@
-import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js";
 
 /**
  * Each link should have a url, a name to display, and a type to control the display coloring
