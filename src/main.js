94ab498f1474ae6824df65a7b3012433f02d212c
LindaKat <lindakt16@gmail.com>
2023-05-29 17:15:11 -0500

    reset router history to hash history

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import './styles.css'
 import App from "./App.vue";
 import HomeView from "./HomeView.vue";
@@ -11,7 +11,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
